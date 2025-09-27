6e3e6a2a49a121725bf63cf1ac718a51f6a3fbc0
yoonwoo123 <lkkjasd@korea.ac.kr>
2025-09-28 06:35:45 +0900

    20250927 solved algorithm add 백준 행렬을 풀어보았는데 최종 return 시 배열로 유효성 검사를 했어야 하는데 하지 않아서 계속 틀렸다..

--- 20250927/행렬.py
+++ 20250927/행렬.py
@@ -1,15 +1,21 @@
 import sys
 sys.stdin = open('행렬.txt')
 
-N, M = map(int, input().split())
+def reverse(i, j):
+    for r in range(i, i + 3):
+        for c in range(j, j + 3):
+            A[r][c] ^= 1
 
+ans = 0
+N, M = map(int, input().split())
 A = [list(map(int, input())) for _ in range(N)]
 B = [list(map(int, input())) for _ in range(N)]
 
-# print(A)
-# print(B)
+for x in range(N - 2):
+    for y in range(M - 2):
+        if A[x][y] != B[x][y]:
+            reverse(x, y)
+            ans += 1
 
-if M < 3:
-    print(-1)
-    exit(0)
+print(ans if A == B else -1)
 
